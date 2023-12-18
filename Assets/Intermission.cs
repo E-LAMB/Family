d85d854551963d152c92138b65e809faa1cd1b10
E-LAMB <aatkinson2022@elam.co.uk>
2023-12-18 12:13:27 +0000

    Intermission

--- Assets/Intermission.cs
+++ Assets/Intermission.cs
@@ -23,12 +23,23 @@ public class Intermission : MonoBehaviour
             tie.SetActive(true);
         }
         big_reveal.SetActive(false);
+
+        if (Mind.player_bro_score > 9 && Mind.player_bro_score != Mind.player_sis_score)
+        {
+            Debug.Log("BRO WINS");
+            timer = -999f;
+        }
+        if (Mind.player_sis_score > 9 && Mind.player_bro_score != Mind.player_sis_score)
+        {
+            Debug.Log("SIS WINS");
+            timer = -999f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        int chosen = Random.Range(2, 7);
+        int chosen = Random.Range(2, 6);
         timer += Time.deltaTime;
         if (Mind.previous_game != chosen && timer > 3f)
         {
